17caf1497d377adde93155ee776453d485d5a9d1
Hussain Aminu <hussain_aminu@yahoo.com>
2020-08-06 22:16:33 +0100

    setup some dummy data

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,9 +1,9 @@
-<h3>Friends</h3>
+<h3 class="font-bold text-xl mb-4">Friends</h3>
 
 <ul>
     <li>
-        <div class="flex">
-            <img src="" alt="">
+        <div class="flex items-center text-sm">
+            <img src="https://i.pravatar.cc/40" alt="John" class="rounded-full mr-2">
             John Doe
         </div>
     </li>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="grid grid-cols-12 gap-4">
-    <div class="col-span-3">
+<div class="lg:grid grid-cols-12 gap-4">
+    <div class="lg:col-span-3">
         @include('_sidebar-links')
     </div>
-    <div class="col-span-6">Hello</div>
-    <div class="col-span-3">
+    <div class="lg:col-span-6">Hello</div>
+    <div class="lg:col-span-3">
         @include('_friends-list')
     </div>
 </div>
