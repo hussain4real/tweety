1d989e99ba0c76eed6f504575f9160b545076a95
Hussain Aminu <hussain_aminu@yahoo.com>
2020-08-24 17:29:28 +0100

    made the user following section dynamic

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,12 +1,12 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4 ">following</h3>
 
 <ul>
-    @foreach (range(1, 8) as $item)
+    @foreach (auth()->user()->follows as $user)
 
     <li class="mb-4">
         <div class="flex items-center text-sm">
-            <img src="https://i.pravatar.cc/40" alt="John" class="rounded-full mr-2">
-            John Doe
+            <img src="{{ $user->avatar }}" alt="John" class="rounded-full mr-2">
+            {{ $user->name }}
         </div>
 
     </li>
