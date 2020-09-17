b19641e31da11adae79c669b7cbf719f9acf146c
Hussain Aminu <hussain_aminu@yahoo.com>
2020-09-18 00:02:40 +0100

    used livewire to make ajax request to post tweet

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,26 +1,8 @@
+{{-- @livewire('tweet') --}}
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
 
-    <form method="POST" action="{{route('tweets.store')}}">
-        @csrf
 
-        {{--<textarea name="body" class="w-full outline-none" placeholder="what's up tweepy?"></textarea>--}}
-        <x-buk-textarea name="body" class="w-full outline-none" placeholder="what's up tweepy?"/>
-
-        <hr class="my-4">
-
-        <footer class="flex justify-between">
-
-            <img src="{{auth()->user()->avatar}}" alt="John" class="rounded-full mr-2" width="50" height="50">
-
-            <button type="submit"
-                class="bg-blue-500 rounded-lg shadow px-2 text-white focus:outline-none">Tweety-a-roo!</button>
-
-        </footer>
-
-
-    </form>
-
-    <x-buk-error field="body" class="text-red-500" />
+    <livewire:tweety />
 
 
 </div>
